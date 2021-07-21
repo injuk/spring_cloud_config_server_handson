76607517903bd903234af63f66235af78f6c2ded
ingnoh1001@gmail.com <ingnog@mz.co.kr>
2021-07-21 14:40:03 +0900

    service discovery

--- src/main/java/com/example/demo/ConfigServer.java
+++ src/main/java/com/example/demo/ConfigServer.java
@@ -4,10 +4,12 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @Log4j2
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServer {
 
   public static void main(String[] args) {
